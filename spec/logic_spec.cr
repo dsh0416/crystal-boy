0f007f2263fe5b4feccd3ea96ffe8d37decb6a7c
Delton Ding <dsh0416@gmail.com>
2017-04-07 20:55:36 +0800

    Using macro for tests

--- spec/logic_spec.cr
+++ spec/logic_spec.cr
@@ -3,67 +3,73 @@ require "cpu"
 
 cpu = CPU.new
 
-describe "Logic" do
-  it "should increase b" do
-    cpu.register.b = 0x0_u8
-    cpu.inc_b
-    cpu.register.b.should eq 0x1
-  end
+macro logic_8bit(names)
+  {% for name in names %}
+    it "should increase {{name}}" do
+      cpu.register.{{name}} = 0x0_u8
+      cpu.inc_{{name}}
+      cpu.register.{{name}}.should eq 0x1
+    end
 
-  it "should decrease b" do
-    cpu.register.b = 0x1_u8
-    cpu.dec_b
-    cpu.register.b.should eq 0x0
-  end
+    it "should decrease {{name}}" do
+      cpu.register.{{name}} = 0x1_u8
+      cpu.dec_{{name}}
+      cpu.register.{{name}}.should eq 0x0
+    end
 
-  it "should sub a and b" do
-    cpu.register.a = 0x2_u8
-    cpu.register.b = 0x1_u8
-    cpu.sub_b
-    cpu.register.a.should eq 0x1
-  end
+    it "should sub a and {{name}}" do
+      cpu.register.a = 0x2_u8
+      cpu.register.{{name}} = 0x1_u8
+      cpu.sub_{{name}}
+      cpu.register.a.should eq 0x1
+    end
 
-  it "should and a and b" do
-    cpu.register.a = 0x1_u8
-    cpu.register.b = 0x1_u8
-    cpu.and_b
-    cpu.register.a.should eq 0x1
-  end
+    it "should and a and {{name}}" do
+      cpu.register.a = 0x1_u8
+      cpu.register.{{name}} = 0x1_u8
+      cpu.and_{{name}}
+      cpu.register.a.should eq 0x1
+    end
 
-  it "should or a and b" do
-    cpu.register.a = 0x0_u8
-    cpu.register.b = 0x1_u8
-    cpu.or_b
-    cpu.register.a.should eq 0x1
-  end
+    it "should or a and {{name}}" do
+      cpu.register.a = 0x0_u8
+      cpu.register.{{name}} = 0x1_u8
+      cpu.or_{{name}}
+      cpu.register.a.should eq 0x1
+    end
 
-  it "should xor a and b" do
-    cpu.register.a = 0x1_u8
-    cpu.register.b = 0x2_u8
-    cpu.xor_b
-    cpu.register.a.should eq 0x3
-  end
+    it "should xor a and {{name}}" do
+      cpu.register.a = 0x1_u8
+      cpu.register.{{name}} = 0x2_u8
+      cpu.xor_{{name}}
+      cpu.register.a.should eq 0x3
+    end
 
-  it "should cp a and b" do
-    cpu.register.a = 0x1_u8
-    cpu.register.b = 0x2_u8
-    cpu.cp_b
-    cpu.register.f = 0b01110000_u8
-  end
+    it "should cp a and {{name}}" do
+      cpu.register.a = 0x1_u8
+      cpu.register.{{name}} = 0x2_u8
+      cpu.cp_{{name}}
+      cpu.register.f = 0b01110000_u8
+    end
 
-  it "should add a and b" do
-    cpu.register.a = 0x1_u8
-    cpu.register.b = 0x2_u8
-    cpu.add_a_b
-    cpu.register.a = 0x3_u8
-  end
+    it "should add a and {{name}}" do
+      cpu.register.a = 0x1_u8
+      cpu.register.{{name}} = 0x2_u8
+      cpu.add_a_{{name}}
+      cpu.register.a = 0x3_u8
+    end
 
-  it "should adc a and b" do
-    cpu.register.set_c
-    cpu.register.a = 0x1_u8
-    cpu.register.b = 0x2_u8
-    cpu.add_a_b
-    cpu.register.a = 0x4_u8
-  end
+    it "should adc a and {{name}}" do
+      cpu.register.set_c
+      cpu.register.a = 0x1_u8
+      cpu.register.{{name}} = 0x2_u8
+      cpu.add_a_{{name}}
+      cpu.register.a = 0x4_u8
+    end
+  {% end %}
+end
+
+describe "Logic" do 
+  logic_8bit([b, c, d, e, h, l])
 end
 
